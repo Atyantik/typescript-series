99876626a5348ce154c6ba3a885134b2a1251a1b
Tirth Bodawala <tirthbodawala@atyantik.com>
2018-12-27 20:50:49 +0530

    Day2- Part2

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 let str: string = "Hello, World!";
-console.log(str);
+function log(s: any): void {
+	const logger: HTMLElement = document.getElementById("logger");
+	const logWrapper: HTMLElement = document.createElement("div");
+	logWrapper.className = "log-entry";
+	logWrapper.innerHTML = `${(new Date).toLocaleTimeString()}: ${JSON.stringify(s)}`;
+	logger.appendChild(logWrapper);
+}
+log(str);
