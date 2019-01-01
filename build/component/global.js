63200e1f0624d54f25e773b830dc031ed1713d51
Tirth Bodawala <tirthbodawala@atyantik.com>
2019-01-01 18:31:52 +0530

    Adding webpack

--- build/component/global.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Global;
-(function (Global) {
-    Global.name = "Tirth";
-})(Global || (Global = {}));
-exports.default = Global;
